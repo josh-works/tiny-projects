eeb9f354253536731c77e24ff85e340495bf3cac
Josh Thompson <thompsonjoshd@gmail.com>
2017-01-12 11:00:37 -0700

    Add Chris Pine, Learn To Program exercises

---
+++ chris_pine_ruby_lessons/fav_number.rb
@@ -0,0 +1,4 @@
+p "what's your favorite number? > "
+fav_num = gets.chomp.to_i
+
+p "oh yeah? well, then, you should like #{fav_num + 1} even more!"
---
+++ chris_pine_ruby_lessons/mixing_it_up.rb
@@ -0,0 +1,9 @@
+
+p "what's your first name? > "
+f_name = gets.chomp
+p "What's your middle name? > "
+m_name = gets.chomp
+p "what's your last name? > "
+l_name = gets.chomp
+
+p "hello and good to meet you, #{f_name} #{m_name} #{l_name}!"
